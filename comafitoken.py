e90de04ae01d9cd89530729497344a3bc7ce9938
Nicolás Alvarez <nicolas.alvarez@gmail.com>
2018-06-15 00:21:02 -0300

    Add module for Comafi Token, and its unit tests

---
+++ comafitoken.py
@@ -0,0 +1,64 @@
+# Copyright (c) 2018 Nicolás Alvarez <nicolas.alvarez@gmail.com>
+# Licensed under the GNU General Public License version 3 or any later version.
+# See LICENSE.txt for details.
+
+
+import time
+import re
+import base64
+
+import requests
+
+import jscrypto
+
+sess = requests.session()
+
+def _fetch_encrypted_payload(activation_code):
+    r = sess.get(
+        "https://tokenv.banelcoservices.com.ar/vuserver/activation.php",
+        params={
+            'cupon': activation_code,
+            'callback': 'callback',
+            '_': str(int(time.time() * 1000))
+        }
+    )
+
+    if r.status_code != 200:
+        raise RuntimeError("status_code not 200") # FIXME better exception
+
+    content_type = r.headers.get('Content-Type', '')
+    if content_type == 'application/javascript':
+        m = re.fullmatch(rb'callback\(\["(.*)"\]\)', r.content)
+        if m is None:
+            raise RuntimeError("raw response has unexpected format")
+
+        b64_data = m.group(1).replace(br'\/', b'/')
+
+        encrypted_blob = base64.b64decode(b64_data)
+        return encrypted_blob
+
+    elif content_type == 'text/html':
+        if r.content == b'URL Incorrecta - contacte Soporte Security':
+            raise RuntimeError("Got 'incorrect url' error - bad token?")
+        else:
+            raise RuntimeError("Got unknown error")
+    else:
+        raise RuntimeError("Got unexpected content type")
+
+def _decrypt_seed(payload, passcode):
+    decrypted_payload = jscrypto.aesCtrDecrypt(payload, passcode, 256)
+
+    m = re.fullmatch(rb'([0-9]+) ([0-9a-zA-Z=]+) ([01]?)', decrypted_payload)
+    if m is None:
+        # likely decryption failed because the passcode is wrong
+        raise RuntimeError("Decryption gave wrong format")
+
+    crc, seed, forcePasscodeReset = m.groups()
+    # TODO verify CRC
+    return seed
+
+def activate(activation_code, passcode):
+
+    encrypted_blob = _fetch_encrypted_payload(activation_code)
+    return _decrypt_seed(encrypted_blob, passcode)
+
---
+++ tests/test_comafitoken.py
@@ -0,0 +1,56 @@
+# Copyright (c) 2018 Nicolás Alvarez <nicolas.alvarez@gmail.com>
+# Licensed under the GNU General Public License version 3 or any later version.
+# See LICENSE.txt for details.
+
+import unittest
+from unittest.mock import patch, MagicMock
+import base64
+
+import comafitoken
+
+TEST_PAYLOAD = b'jNnbU15eXl5dMsxWZ5alkd9nFoWo1Eb1t0Izj4nh5PKVMGI0hOQLBQMv8k2t'
+TEST_SEED = b'KGCQPVRVHCQgTLOeaSLGFTYQC'
+
+class TestBankToken(unittest.TestCase):
+    def setUp(self):
+        self.mock_response = MagicMock(
+            status_code=200,
+            headers={'Content-Type': 'application/javascript'},
+            content=b'callback(["%s"])' % TEST_PAYLOAD
+        )
+
+    @patch('comafitoken.sess.get')
+    def test_fetch(self, mock_get):
+        '''Happy path fetching data'''
+        mock_get.return_value = self.mock_response
+
+        payload = comafitoken._fetch_encrypted_payload('99999999')
+
+        mock_get.assert_called_once()
+        self.assertEqual(mock_get.call_args[1]['params']['cupon'], '99999999')
+        self.assertEqual(mock_get.call_args[1]['params']['callback'], 'callback')
+
+        self.assertEqual(payload, base64.b64decode(TEST_PAYLOAD))
+
+    def test_decrypt(self):
+        '''Happy path decrypting data'''
+        seed = comafitoken._decrypt_seed(base64.b64decode(TEST_PAYLOAD), '999999')
+
+        self.assertEqual(seed, TEST_SEED)
+
+    @patch('comafitoken.sess.get')
+    def test_all(self, mock_get):
+        '''Happy path end to end'''
+
+        mock_get.return_value = self.mock_response
+
+        seed = comafitoken.activate('99999999', '999999')
+
+        mock_get.assert_called_once()
+        self.assertEqual(mock_get.call_args[1]['params']['cupon'], '99999999')
+        self.assertEqual(mock_get.call_args[1]['params']['callback'], 'callback')
+
+        self.assertEqual(seed, TEST_SEED)
+
+if __name__ == '__main__':
+    unittest.main()
